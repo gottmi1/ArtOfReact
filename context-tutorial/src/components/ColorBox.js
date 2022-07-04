9cd3c7abfce5effba606ce0213f1d125141f31b4
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-07-05 04:08:09 +0900

    useContext

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,28 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+// useContext로 Consumer 없이 값을 받아올 수 있음
 import ColorContext, { ColorConsumer } from "../contexts/color";
 
 export default function ColorBox() {
+  const { state } = useContext(ColorContext);
+  // useContext를 사용하면 이렇게 훨씬 보기 쉽고 편해진다. 함수형 컴포넌트에선 이걸 쓰도록 하자.
+
   return (
-    <ColorConsumer>
-      {/* context를 사용할 때, 컨슈머 사이에 중괄호로 jsx나 문자열이 아닌 함수를 넣어주는 패턴을 render Props라고 한다.  */}
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: state.color,
-            }}
-          />
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: state.subcolor,
-            }}
-          />
-        </>
-      )}
-    </ColorConsumer>
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      />
+    </>
   );
 }
